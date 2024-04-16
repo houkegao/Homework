2063af5082eaca1df7d2653077751b6ca0da7dfa
houkegao <156152076+houkegao@users.noreply.github.com>
2024-04-16 14:30:57 -0400

    Add files via upload

---
+++ first -maven project/src/package-info.java
@@ -0,0 +1,43 @@
+ System.out.println("Enter annual internet rate");
+float annualInternetRate= scanner.nextFloat();
+
+ System.out.println("Enter annual internet rate");
+ float annualInternetRate= scanner.nextFloat();
+
+ System.out.println("Enter annual internet rate");
+ public class Financialcalculators{
+     no usages
+     final int MONTH_N_YEAR=12;
+
+     Scnner sanner=new Scanner(System.in);
+
+     System.out.println("Enter annual internet rate");
+     System.out.println("Enter annual internet rate");
+     System.out.println("Enter annual internet rate");
+       int calculator=scanner.nextInt():
+
+       if(calculator==1){}
+               (monthlyInterestRate*(Math.pow(1+monthlyInterestsRate,numberOfPayments))/
+                       (Math.pow(1+monthlyInterestRate,numberOfPayments)-1)
+               );
+       System.out.printLn("Month/payment")
+
+
+
+
+ float annualInternetRate= scanner.nextFloat();
+ int numverOfPayments=termsInYears*12;
+
+      double monthlyPayment=principal*(
+             (monthlyInterestRate*(Math.pow(1+ monthlyInterestRate,numberOfPayment
+                     ((Math.pow(1+monthlyInterestRates,numberOfPayments)-1)
+                     )));
+             System.out.printLn(Monthly payment:"+monthlyPayment"));
+         }else if(calculator==2){
+
+
+
+
+
+
+
